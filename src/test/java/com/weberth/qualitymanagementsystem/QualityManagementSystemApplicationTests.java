3290091e43148346af993c548806625bd9066e2f
Weberth Lopes <weberth.001@gmail.com>
2026-05-20 17:50:26 -0300

    feat: initialize Spring Boot project structure

---
+++ src/test/java/com/weberth/qualitymanagementsystem/QualityManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weberth.qualitymanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QualityManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
